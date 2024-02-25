bb5f6ab84604ec7a7f4602da4bad56a2af54bf2f
aadjones <aaron.demby.jones@gmail.com>
2024-02-25 10:57:12 -0800

    Fix base makefiles to remove OpenMP for now

--- projects/include.mk
+++ projects/include.mk
@@ -1,7 +1,8 @@
-LDFLAGS_COMMON = -framework Accelerate -framework GLUT -framework OpenGL -L/opt/homebrew/lib -lstdc++ -lz -lm -fopenmp
+LDFLAGS_COMMON = -framework Accelerate -framework GLUT -framework OpenGL -L/opt/homebrew/lib -lstdc++ -lz -lm
 LDFLAGS_COMMON += -L/opt/homebrew/opt/libpng/lib -lpng
 LDFLAGS_COMMON += -L/opt/homebrew/opt/jpeg/lib -ljpeg
 LDFLAGS_COMMON += -L/opt/homebrew/opt/fftw/lib -lfftw3
-CFLAGS_COMMON = -v -c -Wall -Wno-deprecated -I../../src/integrators -I../../src/linearalgebra -I../../src/geometry -I../../src/util -I../../src/glvu -DDO_PNG_OUT=0 -I./ -I/opt/homebrew/include -I../../ -I../../src/Eigen/ -O3 -DNO_FFT -fopenmp -lstdc++ -I/opt/local/include/ -flax-vector-conversions
-CFLAGS_COMMON += -fopenmp
+CFLAGS_COMMON = -v -c -Wall -Wno-deprecated -I../../src/integrators -I../../src/linearalgebra -I../../src/geometry -I../../src/util -I../../src/glvu -DDO_PNG_OUT=0 -I./ -I/opt/homebrew/include -I../../ -I../../src/Eigen/ -O3 -DNO_FFT -lstdc++ -I/opt/local/include/ -flax-vector-conversions
+
+
 
